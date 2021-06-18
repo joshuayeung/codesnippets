c9582dd3053d37e5e924dd71fd937238ffa7c359
Joshua Yeung <joshua.chyeung@gmail.com>
2021-06-18 09:25:08 +0800

    Replace consecutive underscores to one single underscore

--- rename_df.py
+++ rename_df.py
@@ -1,4 +1,15 @@
+# Drop all unnamed columns
+del_col = [
+    col for col in df.columns if 'Unnamed' in col]
+df = df.drop(
+    del_col, axis='columns')
+
+# Change all special characters to underscore 
+# Make all letters to lower case
+# Strip the leading and trailing underscores 
+# Replace consecutive underscores to one single underscore
 for counter in df.columns:
-    column_name = ''.join(
-        e if e.isalnum() else "_" for e in counter).lower().strip("_")
-    df.rename(columns={counter: column_name}, inplace=True)
+	column_name = re.sub(r"_{2,}\s*", "_", ''.join(
+		e if e.isalnum() else "_" for e in counter).lower().strip("_"))
+	df.rename(
+		columns={counter: column_name}, inplace=True)
